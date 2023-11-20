eee03d093b781cc857f8f1408a293b541519710f
salvavelazquez <augustorvelazquez@gmail.com>
2023-11-20 01:29:00 -0300

    arreglo

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -8,28 +8,28 @@ import Developers from "./Developers.json";
 
 
 function Developer() {
-    return (
+  return (
 
-        <div id= "ggg" style={{ display: 'flex', flexWrap: 'wrap'/*, justifyContent: 'space-evenly' */}}>
-     
-     {Developers.map((dev) => (
+    <div id="ggg" style={{ display: 'flex', flexWrap: 'wrap', background:"#0b0912", justifyContent:'center' }}>
 
-        <Card key={dev.id} style={{ width: '18rem', margin: '10px' }}>
+      {Developers.map((dev) => (
+
+        <Card key={dev.id} style={{ width: '15rem', margin: '10px', padding:"5px" }}>
           <Card.Img variant="top" src={dev.img} />
           <Card.Body>
             <Card.Title>{dev.name}</Card.Title>
             <Card.Text>
-            
+
               Hobby: {dev.hobby}
-              
+
             </Card.Text>
-            <Button variant="primary" href={dev.repository} target="_blank">Go somewhere</Button>
+            <Button variant="primary" href={dev.repository} target="_blank">REPOSITORIO GIT</Button>
           </Card.Body>
         </Card>
 
-        
-        ))}
-      </div>
-    );
+
+      ))}
+    </div>
+  );
 }
 export default Developer;
